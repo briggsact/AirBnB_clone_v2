e59dad72e41bbd9238886e29e84f7dd76627cf38
briggsact <briggsact@gmail.com>
2022-09-28 22:28:35 +0100

    my commit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,42 +1,31 @@
 #!/usr/bin/python3
 """
-Fabric script (based on the file 1-pack_web_static.py) that distributes
-an archive to your web servers, using the function do_deploy
+Deploying tgz file to our servers
 """
 
-from fabric.api import *
-from datetime import datetime
-import os
-
-filename = "web_static_" + datetime.now().replace(microsecond=0)\
-            .strftime("%Y%m%d%H%M%S") + ".tgz"
-
-env.hosts = ["ubuntu@34.138.32.10", "ubuntu@34.237.53.89"]
-
-
-def do_pack():
-    """Fabric function"""
-    print("Packing web_static to versions/{}".format(filename))
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    local("tar -cvzf versions/{} web_static".format(filename))
-    print("web_static packed: versions/{} -> {}Bytes".format(
-        filename, os.path.getsize("versions/{}".format(filename))))
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['54.144.68.4', '54.88.183.179']
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
-    """Fabric deploy function, takes a file path as argument"""
-    if not os.path.isfile(archive_path):
+    """deploy web static with fabric"""
+    if exists(archive_path) is False:
         return False
-    # remove .tgz extension
-    filename = archive_path.replace("versions/", "")
-    # remove versions folder path
-    folder_name = filename.replace(".tgz", "")
-    put(archive_path, "/tmp", use_sudo=True)
-    run("mkdir -p /data/web_static/releases/{}".format(folder_name))
-    run("tar -xzf /tmp/{} --strip-components 1 -C \
-        /data/web_static/releases/{}".format(filename, folder_name))
-    run("rm /tmp/{}".format(filename))
-    run("rm -rf /data/web_static/current")
-    run("ln -s /data/web_static/releases/{} /data/web_static/current".
-        format(folder_name))
+
+    try:
+        filename = archive_path.split("/")[-1]
+        no_excep = filename.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_excep))
+        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(filename, path, no_excep))
+        run('rm /tmp/{}'.format(filename))
+        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, no_excep))
+        run('rm -rf {}{}/web_static'.format(path, no_excep))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_excep))
+        return True
+    except Exception:
+        return False
